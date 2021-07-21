9f5d9e7c40cc6c6acf1babf70a6df236e0505f3b
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-07-21 19:05:53 +0400

    feat: prepare about-project section

---
+++ src/components/Main/AboutProject/AboutProject.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./AboutProject.css";
+
+function AboutProject(props) {
+  return (
+    <section className="info">
+      <div className="info__container">
+        <div className="info__header">О проекте</div>
+        <div className="info__content">
+          <div className="info__item">
+            <h2 className="info__title">Дипломный проект включал 5 этапов</h2>
+            <p className="info__paragraph">
+              Составление плана, работу над бэкендом, вёрстку, добавление
+              функциональности и финальные доработки.
+            </p>
+          </div>
+          <div className="info__item">
+            <h2 className="info__title">На выполнение диплома ушло 5 недель</h2>
+            <p className="info__paragraph">
+              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+              соблюдать, чтобы успешно защититься.
+            </p>
+          </div>
+        </div>
+        <div className="info__timetable">
+          <div className="info__scale">
+            <div className="info__step info__step_first">1 неделя</div>
+            <div className="info__step info__step_second">4 недели</div>
+          </div>
+          <div className="info__comments">
+            <p className="info__comment">Back-end</p>
+            <p className="info__comment">Front-end</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutProject;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,11 +3,13 @@ import React from "react";
 import "./Main.css";
 
 import Promo from "./Promo/Promo";
+import AboutProject from "./AboutProject/AboutProject";
 
 function Main(props) {
   return (
     <main className="content">
       <Promo />
+      <AboutProject />
     </main>
   );
 }
